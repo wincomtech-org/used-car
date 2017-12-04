fea9adf3061a779f85f26d1282bbea0a20baa829
lenka <1104710647@qq.com>
2017-12-04 17:23:31 +0800

    地图，消息提示音，

--- simplewind/cmf/common.php
+++ simplewind/cmf/common.php
@@ -235,7 +235,7 @@ $html = <<<EOT
         .alert_msg b{position:absolute;top:3px;right:17px;font-size:23px;cursor:pointer;}
     </style>
     <script type="text/javascript">
-        $("body").append("<div class='alert_msg'><b>x</b>{$msg}<br/><a href='{$jumpurl}'>点击查看</a></div>");
+        $("body").append("<div class='alert_msg'><b>x</b>{$msg}<br/><a href='{$jumpurl}'>点击查看</a></div><audio id='sound' autoplay='autoplay' src='__STATIC__/audio/4182.mp3'>");
         // 消息提示弹窗
         $(document).delegate('.alert_msg b','click',function(){
             $(this).parent().hide(600);
