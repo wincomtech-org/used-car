8d13201f68e9ddd8e0c5db2bab68e0cdccd8b945
fengyue <1104710647@qq.com>
2018-01-25 09:39:20 +0800

    333

---
+++ app/user/controller/ShopController.php
@@ -0,0 +1,44 @@
+<?php
+namespace app\user\controller;
+
+use cmf\controller\UserBaseController;
+// use app\user\model\UserModel;
+// use think\Validate;
+use think\Db;
+
+/**
+* 个人中心 服务商城
+*/
+class ShopController extends UserBaseController
+{
+    public function index()
+    {
+        return $this->fetch();
+    }
+
+    public function orders()
+    {
+        return $this->fetch();
+    }
+
+    public function order_table()
+    {
+        return $this->fetch();
+    }
+    public function confimReceipt()
+    {
+        return $this->fetch();
+    }
+    public function evaluate()
+    {
+        return $this->fetch();
+    }
+    public function logistics()
+    {
+        return $this->fetch();
+    }
+    public function shop_cart()
+    {
+        return $this->fetch();
+    }
+}
