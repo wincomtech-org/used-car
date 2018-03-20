c4c00af797a8742fbea9b4848801d7cc07f32d2f
luosader <greatsen@sina.com>
2018-03-20 19:26:45 +0800

    3

--- public/themes/datong_car/public/assets/js/lanrenzhijia.js
+++ public/themes/datong_car/public/assets/js/lanrenzhijia.js
@@ -1,8 +1,8 @@
 ﻿
 //鼠标经过预览图片函数
 function preview(img){
-	$("#preview .jqzoom img").attr("src",$(img).attr("src"));
-	$("#preview .jqzoom img").attr("jqimg",$(img).attr("bimg"));
+	$("#preview .jqzoom img").attr("src",$(img).attr("bimg"));
+	$("#preview .jqzoom img").attr("jqimg",$(img).attr("orginImg"));
 }
 //图片放大镜效果
 // ipad以下的尺寸没有放大镜效果
